ce0e4756b57482a12e74a3a5c3ddf253671349e0
hakim <hakimsembiring@outlook.co.id>
2019-11-14 04:24:26 +0000

    Input Validation Error Handling

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -36,6 +37,16 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $rules = [
+            'title' => ['required']
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
         $data = new Product();
         $data->title = $request->get('title');
         $data->price = $request->get('price');
@@ -90,6 +101,16 @@ class ProductController extends Controller
 
         if (is_null($data)) {
             return response()->json(null, 404);
+        }
+
+        $rules = [
+            'title' => ['required']
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
 
         $data->title = $request->get('title');
