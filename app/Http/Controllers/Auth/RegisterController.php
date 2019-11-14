749c404f446e7c145538e2f97fbb4ef5cf20b517
hakim <hakimsembiring@outlook.co.id>
2019-11-14 06:49:33 +0000

    API Authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -67,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'api_token' => Str::random(80),
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('product', 'ProductController@index');
-Route::post('product', 'ProductController@store');
-Route::get('product/{id}', 'ProductController@show');
-Route::put('product/{id}', 'ProductController@update');
-Route::delete('product/{id}', 'ProductController@destroy');
+// Route::get('product', 'ProductController@index');
+// Route::post('product', 'ProductController@store');
+// Route::get('product/{id}', 'ProductController@show');
+// Route::put('product/{id}', 'ProductController@update');
+// Route::delete('product/{id}', 'ProductController@destroy');
 
 
-// Route::resource('product', 'ProductController')->except([
-//     'create', 'edit'
-// ]);
+Route::resource('product', 'ProductController')->except([
+    'create', 'edit'
+])->middleware('auth:api');
